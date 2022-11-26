1bb8ddd41cf96ec794746dba86fe867e12050704
Tomas Garza <tomas.arangua@hotmail.com>
2022-11-26 12:25:34 -0600

    fd

--- racing_wheel.py
+++ racing_wheel.py
@@ -107,8 +107,8 @@ class RacingWheel:
                 await self.handleButton('btn_lb', 'l', hexData_lr_dir & 0b1000000)
                 await self.handleButton('btn_rb', 'r', hexData_lr_dir & 0b10000000)
 
-            print(int(hexDat[6]))
-            print(int(hexDat[7]))
+            print(int(hexData[6]))
+            print(int(hexData[7]))
             print('---------')
             #print( str(( int(hexData[6]) << 8) | int(hexData[7]))))
 
